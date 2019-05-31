82f66d1749268051a03699e016a0781ef80baee9
Chayan Datta <sonicxxx7@gmail.com>
2019-05-31 22:36:52 +0530

    Navbar added

--- customer_front/src/App.vue
+++ customer_front/src/App.vue
@@ -1,8 +1,27 @@
-<template>
+<<<template>
   <div id="app">
-    <HomeComponent />
+    <div class="container">
+      <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/create" class="nav-link">Create Post</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/posts" class="nav-link">Posts</router-link>
+          </li>
+        </ul>
+      </nav>
+      <br />
+      <transition name="fade">
+        <router-view></router-view>
+      </transition>
+    </div>
   </div>
 </template>
+
 
 <script>
 import HomeComponent from './components/HomeComponent.vue'
@@ -25,4 +44,12 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s
+}
+.fade-enter, .fade-leave-active {
+  opacity: 0
+}
+
 </style>
