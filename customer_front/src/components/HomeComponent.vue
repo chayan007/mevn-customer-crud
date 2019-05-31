49398da7af949ff2b4a6976aea15b0afef95aa0a
Chayan Datta <sonicxxx7@gmail.com>
2019-05-31 22:31:15 +0530

    Base components structure added

--- customer_front/src/components/HomeComponent.vue
+++ customer_front/src/components/HomeComponent.vue
@@ -1,7 +1,16 @@
 <template>
-    
-</template>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card card-default">
+                <div class="card-header">Home Component</div>
 
+                <div class="card-body">
+                    I'm the Base Component component.
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
 <script>
     export default {
         name: "HomeComponent"
