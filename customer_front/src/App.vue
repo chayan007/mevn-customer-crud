a9ed40dce985f9fb527a45e60fff68ae86573f00
Chayan Datta <sonicxxx7@gmail.com>
2019-05-31 22:42:47 +0530

    Basic form created

--- customer_front/src/App.vue
+++ customer_front/src/App.vue
@@ -1,4 +1,4 @@
-<<<template>
+<template>
   <div id="app">
     <div class="container">
       <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
--- customer_front/src/components/CreateComponent.vue
+++ customer_front/src/components/CreateComponent.vue
@@ -1,20 +1,43 @@
 <template>
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card card-default">
-                <div class="card-header">Create Component</div>
-
-                <div class="card-body">
-                    I'm the Create Component component.
+        <h1>Create A Post</h1>
+        <form @submit.prevent="addPost">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label>User Name:</label>
+                        <input type="text" class="form-control" v-model="user.name">
+                    </div>
                 </div>
             </div>
-        </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label>User EMail:</label>
+                        <textarea class="form-control" v-model="user.email" rows="5"></textarea>
+                    </div>
+                </div>
+            </div><br />
+            <div class="form-group">
+                <button class="btn btn-primary">Create</button>
+            </div>
+        </form>
     </div>
 </template>
 
 <script>
     export default {
-        name: "CreateComponent"
+        name: CreateComponent,
+        data(){
+            return {
+                user:{}
+            }
+        },
+        methods: {
+            addPost(){
+                console.log(this.post);
+            }
+        }
     }
 </script>
 
