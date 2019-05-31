09c7b66bf8f5e394ebe715526293e7c9f5aa92f2
Chayan Datta <sonicxxx7@gmail.com>
2019-05-31 23:09:36 +0530

    Server updated and router added in it

--- Customer_Back/server.js
+++ Customer_Back/server.js
@@ -5,16 +5,19 @@ const PORT = 4000;
 const cors = require('cors');
 const mongoose = require('mongoose');
 const config = require('./DB.js');
+const userRoute = require('./user.route');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DB, { useNewUrlParser: true }).then(
-    () => {console.log('Database is connected') },
+    () => { console.log('Database is connected') },
     err => { console.log('Can not connect to the database'+ err)}
 );
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.use('/posts', userRoute);
 
 app.listen(PORT, function(){
     console.log('Server is running on Port:',PORT);
